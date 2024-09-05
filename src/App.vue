b8657c8776660e85d3863b4dc963b8fde9e51a02
chanhee han <mh981219@naver.com>
2024-09-05 13:07:31 +0900

    1 vue 쓰는 이유 , 2 vue3버전 설치 세팅 , 3 데이터바인딩 문법 ,4 v-for 반복문

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="menu">
+    <a v-for="(a) in menu" :key="a">{{a}}</a>
+  </div>
+
+  <div>
+    <h4 v-for="(name,i) in products" :key="name">{{products[i]}}</h4>
+    <p>70 만원</p>
+  </div>
+  <div>
+    <h4 class="red" :style="style">{{products[0]}}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>70 만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>90 만원</p>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      menu : ['Home', 'Products', 'About'],
+      products : ['역삼동 원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +45,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
 </style>
